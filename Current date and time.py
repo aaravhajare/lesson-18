18137238ef240eba3669f28074aaabcac9fbf527
aaravhajare <hajareaarav66@gmail.com>
2025-03-31 18:00:31 +0530

    Current date and time

---
+++ Current date and time.py
@@ -0,0 +1,9 @@
+from datetime import date , time, datetime
+
+today = date.today()
+now = datetime.now()
+
+print("today's date is " , today)
+print("current tie is " , now)
+
+print(today.year , today.month , today.day)
